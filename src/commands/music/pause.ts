61b5bd0e61f6c57d178c886aa715f35b7a41f366
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-09-14 20:44:50 +0200

    feature/pause song command

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,28 @@
+import { ICommand } from "../../interfaces";
+
+export const command: ICommand = {
+  name: "pause",
+  aliases: [],
+  run: async (client, message, args) => {
+    if (!message.member.voice.channel)
+      return message.channel.send(`You're not in a voice channel !`);
+
+    if (
+      message.guild.me.voice.channel &&
+      message.member.voice.channel.id !== message.guild.me.voice.channel.id
+    )
+      return message.channel.send(`You are not in the same voice channel !`);
+
+    if (!client.player.getQueue(message))
+      return message.channel.send(`No music currently playing !`);
+
+    if (client.player.getQueue(message).paused)
+      return message.channel.send(`The music is already paused !`);
+
+    client.player.pause(message);
+
+    message.channel.send(
+      ` Song ${client.player.getQueue(message).playing.title} paused !`
+    );
+  },
+};
