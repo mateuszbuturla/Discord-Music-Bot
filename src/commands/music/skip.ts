e6fcee01f3edc01cc5bd2c57fd82e8bd7127181a
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-09-14 20:49:27 +0200

    feature/skip command

---
+++ src/commands/music/skip.ts
@@ -0,0 +1,23 @@
+import { ICommand } from "../../interfaces";
+
+export const command: ICommand = {
+  name: "skip",
+  aliases: [],
+  run: async (client, message, args) => {
+    if (!message.member.voice.channel)
+      return message.channel.send(`You're not in a voice channel !`);
+
+    if (
+      message.guild.me.voice.channel &&
+      message.member.voice.channel.id !== message.guild.me.voice.channel.id
+    )
+      return message.channel.send(`You are not in the same voice channel !`);
+
+    if (!client.player.getQueue(message))
+      return message.channel.send(`No music currently playing !`);
+
+    client.player.skip(message);
+
+    message.channel.send(`The current music has just been **skipped** !`);
+  },
+};
