0410d082e46668f3a919bd1a25f19413b8966599
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-10-18 21:05:26 +0200

    feature/created embed generator

--- src/helpers/commandNotFound.ts
+++ src/helpers/commandNotFound.ts
@@ -1,20 +1,16 @@
 import { MessageEmbed } from "discord.js";
 import ConfigJSON from "../config.json";
 import { IConfig } from "../interfaces/Config.interface";
+import { EmbedType } from "../interfaces/Embed.interface";
+import { generateEmber } from "../utils/generateEmbed";
 
 export const commandNotFound = (client, message) => {
   const config: IConfig = ConfigJSON;
 
-  const embed = new MessageEmbed();
-
-  embed.setColor("RED");
-  embed.setAuthor(
-    client.user.username,
-    client.user.displayAvatarURL({ size: 1024, dynamic: true })
-  );
-  embed.setDescription(
-    `Unknown command. Type ${config.prefix}help to show commands list.`
-  );
+  const embed: MessageEmbed = generateEmber(client, {
+    type: EmbedType.ERROR,
+    description: `Unknown command. Type ${config.prefix}help to show commands list.`,
+  });
 
   message.channel.send(embed).then((msg) => {
     setTimeout(() => msg.delete(), 5000);
