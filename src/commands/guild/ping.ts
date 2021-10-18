9e62c0f32743cca7eb2e36f9fc7fbe36e356c39c
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-10-18 21:44:34 +0200

    refactor/changed message for ping command

--- src/commands/guild/ping.ts
+++ src/commands/guild/ping.ts
@@ -1,9 +1,18 @@
+import { MessageEmbed } from "discord.js";
 import { ICommand } from "../../interfaces";
+import { EmbedType } from "../../interfaces/Embed.interface";
+import { generateEmber } from "../../utils/generateEmbed";
+import { sendMessage } from "../../utils/sendMessage";
 
 export const command: ICommand = {
   name: "ping",
-  aliases: ["p"],
+  aliases: [],
   run: async (client, message, args) => {
-    message.channel.send(`${client.ws.ping} ping`);
+    const embed: MessageEmbed = generateEmber(client, {
+      type: EmbedType.SUCCESS,
+      description: `${client.ws.ping} ping`,
+    });
+
+    sendMessage(message, embed);
   },
 };
